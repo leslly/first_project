bde766d3fdba929b0d78e9b778205888858929b1
leslly <leslican101@gmail.com>
2023-02-07 11:43:16 +0100

    print

--- bin/first_project.dart
+++ bin/first_project.dart
@@ -3,3 +3,8 @@ import 'package:first_project/first_project.dart' as first_project;
 void main(List<String> arguments) {
   print('Hello world: ${first_project.calculate()}!');
 }
+
+void gene(){
+  print("Iam a genie from a lamp");
+}
+
